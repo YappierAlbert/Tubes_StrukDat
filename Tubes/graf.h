b9cb162bc8804032dd4bc4c5418c1edb8c94544f
Yappier Albertus <yappier.xrpl37@gmail.com>
2024-12-28 12:11:13 +0700

    Add files via upload

---
+++ Tubes/graf.h
@@ -0,0 +1,59 @@
+#ifndef GRAF_H_INCLUDED
+#define GRAF_H_INCLUDED
+
+#include <iostream>
+
+#define firstVertex(G) G.firstVertex
+#define firstEdge(E) E->firstEdge
+#define nextVertex(V) V->next
+#define nextEdge(E) E->nextEdge
+
+using namespace std;
+
+typedef struct vertex *adrVertex;
+typedef struct edge *adrEdge;
+
+struct vertex {
+    string idVertex;
+    adrVertex next;
+    adrEdge firstEdge;
+};
+
+struct edge {
+    string destVertexID;
+    int gold;
+    int xp;
+    int jarak;
+    bool hidup;
+    adrEdge nextEdge;
+};
+
+struct graph {
+    adrVertex firstVertex;
+};
+
+void createVertex(string newVertexID, adrVertex &v);
+
+void initGraph(graph &G);
+
+void addVertex(graph &G, string newVertexID);
+
+void buildGraph(graph &G);
+
+void showVertex(graph G);
+
+adrVertex searchVertex(graph G, string vertexID);
+
+void createEdge(graph &G, string vertexId, string destVertexId, int gold, int xp, int jarak, bool hidup);
+
+void connectVertex(graph &G, string vertexId, string destVertexId, int gold, int xp, int jarak, bool hidup);
+
+void initEdge(graph &G);
+
+void shortestRoute(graph &G, string startVertex);
+
+void highestXp(graph &G, string startVertex);
+
+void highestGold(graph &G, string startVertex);
+
+#endif // GRAF_H_INCLUDED
---
+++ Tubes/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+#include "graf.h"
+
+void menu(){
+    cout << "=====================================" << endl;
+    cout << "       Rute Farm Mobile Legend       " << endl;
+    cout << "=====================================" << endl;
+    cout << "1.Print Graf             " << endl;
+    cout << "2.Rute Farming           " << endl;
+    cout << "3.Rute XP Terbanyak      " << endl;
+    cout << "4.Rute Gold Terbanyak    " << endl;
+    cout << "0.Exit                   " << endl;
+    cout << "=====================================" << endl;
+    cout << "Masukkan Inputan : ";
+}
+
+int main() {
+    string input;
+    int pilih;
+    graph G;
+    initGraph(G);
+    buildGraph(G);
+    initEdge(G);
+    menu();
+    cin >> pilih;
+    while(pilih != 0){
+        if(pilih == 1){
+            showVertex(G);
+        }else if(pilih == 2){
+            cout << "Masukkan Titik Awal Anda : ";
+            cin >> input;
+            shortestRoute(G,input);
+        }else if(pilih == 3){
+            cout << "Masukkan Titik Awal Anda : ";
+            cin >> input;
+            initEdge(G);
+            highestXp(G,input);
+        }else if(pilih == 4){
+            cout << "Masukkan Titik Awal Anda : ";
+            cin >> input;
+            initEdge(G);
+            highestGold(G,input);
+        }else{
+            cout << "Input Salah!!" << endl;
+        }
+        menu();
+        cin >> pilih;
+    }
+
+    return 0;
+}
